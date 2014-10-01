1a20c94dc6edd30dffe6d36a901da3a1791fe363
Zsolt Lengyel <zsolt.lengyel.it@gmail.com>
2014-10-01 09:00:49 +0200

    google-auth module

---
+++ src/main/java/com/codecity/web/modules/auth/modules/google/controllers/IndexController.java
@@ -0,0 +1,12 @@
+package com.codecity.web.modules.auth.modules.google.controllers;
+
+import io.pallas.core.annotations.Controller;
+
+@Controller
+public class IndexController {
+
+	public String authGoogle(){
+		return "google auth";
+	}
+
+}
---
+++ src/main/java/com/codecity/web/modules/auth/modules/google/package-info.java
@@ -0,0 +1,9 @@
+/**
+ *
+ */
+/**
+ * @author lzsolt
+ *
+ */
+@io.pallas.core.annotations.Module("google-auth")
+package com.codecity.web.modules.auth.modules.google;
