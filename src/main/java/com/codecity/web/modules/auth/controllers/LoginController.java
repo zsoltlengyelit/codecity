9ca43b12e17a6d32ec7a13bb90a4225b76178a59
Zsolt Lengyel <zsolt.lengyel.it@gmail.com>
2014-10-08 20:25:23 +0200

    Base shiro authentication

--- src/main/java/com/codecity/web/modules/auth/controllers/LoginController.java
+++ src/main/java/com/codecity/web/modules/auth/controllers/LoginController.java
@@ -11,43 +11,94 @@ import io.pallas.core.view.form.Form;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.ExcessiveAttemptsException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.LockedAccountException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+
 import com.codecity.web.controller.IndexController;
 import com.codecity.web.modules.auth.form.LoginForm;
 
 @Controller
 public class LoginController extends BaseController {
 
-	@Inject
-	private Template template;
+    @Inject
+    private Template template;
 
-	@Inject
-	private HttpRequest request;
+    @Inject
+    private HttpRequest request;
 
-	@PostConstruct
-	private void init() {
-		template.setPath("layout/login");
-	}
+    @PostConstruct
+    private void init() {
+        template.setPath("layout/login");
+    }
 
-	/**
-	 * @return view
-	 */
-	public Result index() {
+    /**
+     * @return view
+     */
+    public Result index() {
 
-		final Model viewModel = new Model();
-		Form<LoginForm> form = Form.from(LoginForm.class);
+        final Model viewModel = new Model();
+        Form<LoginForm> form = Form.from(LoginForm.class);
 
-		if (request.isPostMethod()) {
-			form = form.bindFromRequest();
+        if (request.isPostMethod()) {
+            form = form.bindFromRequest();
 
+            if (!form.hasErrors()) {
 
-			if (!form.hasErrors()) {
+                final LoginForm loginForm = form.get();
+                final UsernamePasswordToken token = loginForm.asToken();
+                if (authentiacte(token)) {
 
-				LoginForm loginForm = form.get();
+                    SecurityUtils.getSubject().getSession().getAttribute("flash");
+                    return redirect(IndexController.class); // login is successful
+                }
+            }
+        }
 
-				return redirect(IndexController.class); // login is successful
-			}
-		}
+        return view("modules/auth/index/index").set("form", form).set(viewModel);
+    }
 
-		return view("modules/auth/index/index").set("form", form).set(viewModel);
-	}
+    /**
+     * Tries authenticate with token.
+     *
+     * @param token
+     * @return
+     */
+    private boolean authentiacte(final UsernamePasswordToken token) {
+
+        //1.
+        final Factory<org.apache.shiro.mgt.SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+
+        //2.
+        final org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
+
+        //3.
+        SecurityUtils.setSecurityManager(securityManager);
+
+        final Subject currentUser = SecurityUtils.getSubject();
+
+        try {
+            currentUser.login(token);
+
+            return true;
+        } catch (final UnknownAccountException uae) {
+            System.out.println(uae);
+        } catch (final IncorrectCredentialsException ice) {
+            System.out.println(ice);
+        } catch (final LockedAccountException lae) {
+            System.out.println(lae);
+        } catch (final ExcessiveAttemptsException eae) {
+            System.out.println(eae);
+        } catch (final AuthenticationException ae) {
+            System.out.println(ae);
+        }
+        return false;
+    }
 }
--- src/main/java/com/codecity/web/modules/auth/modules/google/GoogleAuthModule.java
+++
@@ -1,8 +0,0 @@
-package com.codecity.web.modules.auth.modules.google;
-
-import io.pallas.core.module.Module;
-
-@io.pallas.core.annotations.Module
-public class GoogleAuthModule extends Module{
-
-}
